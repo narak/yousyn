0566f2bd20d9f37efd3e4c9854d220190e2d0294
Satish Srinivasan <satish@vasan.me>
2014-01-30 12:26:22 +0530

    See thumbnail images of the vids

--- static/js/search.js
+++ static/js/search.js
@@ -11,7 +11,31 @@ IMCoop.search = (function() {
 
   addToPlaylistFn = function(evt) {
     evt.preventDefault();
-    IMCoop.playlist.add(searchResults[this.getAttribute('href').substr(1)]);
+    IMCoop.playlist.add(searchResults[this.parentNode.getAttribute('href').substr(1)]);
+  };
+
+  displayImageThumbs = function(evt) {
+    // XXX: can't i use [].filter(fn)?
+
+    var thPresent = false;
+    var children = this.children
+    var thumb = null;
+
+    for(var i in children) {
+      if(children[i].className == "thumbimg") {
+        thPresent = true;
+        thumb = children[i]
+      }
+    }
+
+    var getImgs = function (x) { return [0,1,2,3].map(function(y) { return "http://img.youtube.com/vi/"+x+"/"+y+".jpg"})}
+
+    thumbs = getImgs(this.getAttribute('href').substr(1));
+    if(thPresent == false) {
+      this.innerHTML += "<img class='thumbimg' src='"+thumbs[0]+"'>";
+    } else {
+      thumb.remove();
+    }
   };
 
   /**
@@ -38,7 +62,8 @@ IMCoop.search = (function() {
       return false;
     });
 
-    alf.event.on(IMCoopConfig.el.searchResults, 'click', 'a', addToPlaylistFn);
+    alf.event.on(IMCoopConfig.el.searchResults, 'click', 'i', addToPlaylistFn);
+    alf.event.on(IMCoopConfig.el.searchResults, 'click', 'a', displayImageThumbs);
 
     alf.dom.each(document.querySelectorAll('.show-on-load'), function(el) {
       el.classList.remove('show-on-load');
